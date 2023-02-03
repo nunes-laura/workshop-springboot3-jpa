7001980b13ed85ff031196b7288cf858880c634e
Laura Nunes <lauranunes2508@gmail.com>
2023-02-03 14:36:07 -0300

    Product

---
+++ src/main/java/com/webservices/course/resources/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.webservices.course.resources.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservices.course.entities.Product;
+
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+
+}
